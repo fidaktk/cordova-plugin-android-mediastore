e80b5e29058f16c2f58c626949bc84da8badef84
fidaktk <fidaktk@gmail.com>
2023-06-18 15:33:30 +0500

    Update CordovaAndroidMediaStore.js

--- www/CordovaAndroidMediaStore.js
+++ www/CordovaAndroidMediaStore.js
@@ -1,8 +1,40 @@
-var exec = require('cordova/exec');
+/*global cordova, module*/
 
-exports.store = function (byteString, fileDir, fileName, type, success, error) {
-    exec(success, error,
-      'CordovaAndroidMediaStore',
-      'store',
-      [byteString, fileDir, fileName, type]);
-};
+module.exports = (function() {
+  function callPromise(name) {
+    return function(...params) {
+      return new Promise((resolve, reject) =>
+        cordova.exec(resolve, reject, 'CordovaAndroidMediaStore', this.name, params)
+      );
+    }.bind({ name });
+  }
+
+  let exports = {};
+
+  [
+    'selectFolder',
+    'selectFile',
+    'openFolder',
+    'openFile',
+    'readFile',
+    'writeFile',
+    'overwriteFile',
+    'saveFile',
+    'deleteFile',
+    'getFileName',
+    'getUri'
+  ].forEach(action => (exports[action] = callPromise(action)));
+
+  exports.store = function(byteString, fileDir, fileName, type) {
+    return new Promise((resolve, reject) =>
+      cordova.exec(resolve, reject, 'CordovaAndroidMediaStore', 'store', [
+        byteString,
+        fileDir,
+        fileName,
+        type
+      ])
+    );
+  };
+
+  return exports;
+})();
